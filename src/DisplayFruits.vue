45b8a5baa01a63387a2005ca08aeab09541f0e2b
vitalii boiko <vukelan@gmail.com>
2025-02-13 23:59:17 +0200

    center column

--- src/DisplayFruits.vue
+++ src/DisplayFruits.vue
@@ -5,7 +5,7 @@ const { data } = defineProps(['data']);
 
 <template>
   <div :class="$style.outerContainer">
-    <div>
+    <div :class="$style.mainColumn">
       <div v-for="(fruit, index) in data" :key="index" :class="$style.cardWrap">
         <FruitCard :fruit />
       </div>
@@ -16,6 +16,12 @@ const { data } = defineProps(['data']);
 <style module>
 .outerContainer {
   display: grid;
+  grid-template-columns: 2rem auto 2rem;
+}
+.mainColumn {
+  grid-column-start: 2;
+  grid-column-end: 2;
+  justify-self: center;
 }
 .cardWrap {
   width: fit-content;
