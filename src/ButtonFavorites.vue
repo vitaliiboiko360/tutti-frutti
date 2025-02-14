c8d53fc0b45eb0e651be48fefbb690e2d06d5404
vitalii boiko <vukelan@gmail.com>
2025-02-15 00:27:38 +0200

    add link to favourites

--- src/ButtonFavorites.vue
+++ src/ButtonFavorites.vue
@@ -1,21 +1,45 @@
 <script setup>
 import { ref } from 'vue';
 
+const isSelected = ref();
 const refImg = ref();
 const onClick = () => {
-  console.log(refImg.value);
+  if (isSelected.value) {
+    refImg.value.setAttribute('src', 'star.svg');
+    isSelected.value = false;
+  } else {
+    refImg.value.setAttribute('src', 'selectedStar.svg');
+    isSelected.value = true;
+  }
 };
 </script>
 
 <template>
-  <div>
+  <div :class="$style.favouritesBlock">
     <button @click="onClick" :class="$style.buttonFavourites">
-      <img :ref="(el) => (refImg = el)" src="star.svg" />
+      <img :ref="(el) => (refImg = el)" :src="`star.svg`" />
     </button>
+    <div :class="[$style.linkFavourites, { [$style.hidden]: !isSelected }]">
+      <NuxtLink to="/favourites">favourites</NuxtLink>
+    </div>
   </div>
 </template>
 
 <style module>
+.favouritesBlock {
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  width: 2.5rem;
+}
+.hidden {
+  display: none;
+}
+.linkFavourites {
+  position: absolute;
+  right: -0.3rem;
+  bottom: -1rem;
+}
 .buttonFavourites {
   cursor: pointer;
   border: 0;
