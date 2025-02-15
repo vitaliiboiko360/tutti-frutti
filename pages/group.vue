ec2ac6444000fb7c7b0079c5246efee2e85b142c
vitalii boiko <vukelan@gmail.com>
2025-02-15 15:47:23 +0200

    add route group

---
+++ pages/group.vue
@@ -0,0 +1,8 @@
+<script setup>
+import FruitsGroupEntryPoint from '~/src/FruitsGroupEntryPoint.vue';
+
+const route = useRoute();
+const groupName = route.query.name;
+</script>
+
+<template><FruitsGroupEntryPoint :groupName /></template>
